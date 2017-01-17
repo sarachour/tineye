5cf1fc85fd92be49e06e3777c1e5ee9ff826696e
sachour <sachour@mit.edu>
2017-01-17 14:54:48 -0500

    Added

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 from contract import *
 import seaborn as sns
+import numpy as np
 
 class SummaryVis:
     def __init__(self):
@@ -7,18 +8,33 @@ class SummaryVis:
         self._db.read();
 
     @property
-    def db():
+    def db(self):
         return self._db
 
     def wallet_breakdown(self,output):
         vls = [];
+        empty = 0;
+        nonempty = 0;
         for addr in self.db.contracts:
             contract = self.db.contracts[addr]
-            vls.append(contract.wallet);
+            if contract.wallet > 0:
+                vls.append(contract.wallet);
+                nonempty += 1;
+            else:
+                empty += 1;
 
-        fig = sns.kdeplot(vls,shade=True)
-        fig.safefig(output);
-        raise("todo: wallet breakdown")
+        print("empty wallets:"+str(empty));
+        print("nonempty wallets:"+str(nonempty))
+        #snsplot = sns.boxplot(np.array(vls),showfliers=False);
+        #snsplot = sns.boxplot(np.array(vls));
+        snsplot = sns.distplot(np.array(vls));
+        #snsplot = sns.boxplot(np.array(vls));
+        #snsplot = sns.kdeplot(np.array(vls),shade=True)
+        #snsplot.set(xscale='log')
+        #snsplot.set(xlim=(0,1000))
+        snsplot.set(xlabel="Ether in Wallet")
+        fig = snsplot.get_figure()
+        fig.savefig(output);
 
     def execute(self,name,output):
         if name == "wallet":
